9dac05203688e72491f16611b59950b8c541541b
Rebbekha <61548418+Rebbekha@users.noreply.github.com>
2024-11-03 22:57:53 +0100

    Add the new event

--- src/events/events.lua
+++ src/events/events.lua
@@ -28,4 +28,7 @@ E.Wow = {
   EncounterLootReceived = "ENCOUNTER_LOOT_RECEIVED",
   PlayerLogin = "PLAYER_LOGIN",
   PlayerLogout = "PLAYER_LOGOUT",
+  ChatMsgLoot = "CHAT_MSG_LOOT",
+  LootHistoryUpdateDrop = "LOOT_HISTORY_UPDATE_DROP",
+  GroupLeft = "GROUP_LEFT"
 }
