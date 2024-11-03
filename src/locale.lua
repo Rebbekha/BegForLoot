f986fffdc35f0ff6cd8cf52d2ba447db556e06c0
Rebbekha <61548418+Rebbekha@users.noreply.github.com>
2024-11-03 09:09:40 +0100

    Update locale.lua

--- src/locale.lua
+++ src/locale.lua
@@ -20,7 +20,7 @@ L["MINIMAP_ICON_TEXT_TOOLTIP"] = "Enable an icon on the minimap."
 L["PROMOTE_ADDON_TEXT"] = "Promote the Addon"
 L["PROMOTE_ADDON_TEXT_TOOLTIP"] = "If the option is activated, the name of the Addon (<Beg for Loot>) will be put at the beginning of each message sent by the addon."
 L["GROUP_LOOT_TEXT"] = "Group Loot Rolls"
-L["GROUP_LOOT_TEXT_TOOLTIP"] = "Enable the option to whisper the winner of group loot rolls."
+L["GROUP_LOOT_TEXT_TOOLTIP"] = "Experimental Function : Enable the option to whisper the winner of group loot rolls."
 
 
   --Offering Loot
