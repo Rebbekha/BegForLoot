0d53a2a072431d9c55f96afe6bb4a3e039e46546
Rebbekha <61548418+Rebbekha@users.noreply.github.com>
2024-11-04 17:44:34 +0100

    Update saved-variables.lua

--- src/saved-variables.lua
+++ src/saved-variables.lua
@@ -44,16 +44,10 @@ local function globalDefaults()
 
   TextWhisper1 = L.WHISPER_EDITBOX_1,
   TextWhisper2 = L.WHISPER_EDITBOX_2,
-  TextWhisper3 = L.WHISPER_EDITBOX_3,
+  TextWhisper3 = L.WHISPER_EDITBOX_3
   }
 end
---[[
-local function perCharDefaults()
-  local t = globalDefaults()
-  t.characterSpecificSettings = false
-  return t
-end
-]]
+
 local function populate(t, defaults)
   for k, v in pairs(defaults) do
     if type(v) == "table" then
@@ -83,11 +77,8 @@ end
 -- Initialize.
 EventManager:Once(E.Wow.PlayerLogin, function()
   if type(_G[GLOBAL_SV]) ~= "table" then _G[GLOBAL_SV] = {} end
- -- if type(_G[PERCHAR_SV]) ~= "table" then _G[PERCHAR_SV] = {} end
   populate(_G[GLOBAL_SV], globalDefaults())
---  populate(_G[PERCHAR_SV], perCharDefaults())
   local global = _G[GLOBAL_SV]
- -- local perChar = _G[PERCHAR_SV]
 
   function SavedVariables:Get()
     return global
@@ -96,26 +87,13 @@ EventManager:Once(E.Wow.PlayerLogin, function()
   function SavedVariables:GetGlobal()
     return global
   end
-  -- function SavedVariables:GetPerChar()
-    -- return perChar
-  -- end
 
-  -- function SavedVariables:IsGlobal()
-    -- return perChar.characterSpecificSettings
-  -- end
-
-  -- function SavedVariables:Switch()
-    -- perChar.characterSpecificSettings = not perChar.characterSpecificSettings
-    -- EventManager:Fire(E.SavedVariablesSwitched)
-  -- end
 
   EventManager:Fire(E.SavedVariablesReady)
-  --EventManager:Fire(E.SavedVariablesSwitched)
+
 end)
 
 -- Deinitialize.
 EventManager:On(E.Wow.PlayerLogout, function()
-  
   depopulate(_G[GLOBAL_SV], globalDefaults())
-  --depopulate(_G[PERCHAR_SV], perCharDefaults())
 end)
